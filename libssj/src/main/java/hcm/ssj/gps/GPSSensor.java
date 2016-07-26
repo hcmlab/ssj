57cd0ce67c749fce872c6ec655f139bfe176aaed
Michael Dietz <michael.dietz@informatik.uni-augsburg.de>
2016-07-26 11:25:03 +0200

    GPS sensor now does not crash if GPS is turned off and ignoreData flag is set

--- libssj/src/main/java/hcm/ssj/gps/GPSSensor.java
+++ libssj/src/main/java/hcm/ssj/gps/GPSSensor.java
@@ -100,7 +100,7 @@ public class GPSSensor extends Sensor
 				}
 			}
 
-			if (listener.receivedData || options.ignoreData.get())
+			if (listener.receivedData)
 			{
 				connected = true;
 			}
@@ -108,6 +108,11 @@ public class GPSSensor extends Sensor
 			{
 				Log.e("unable to connect to gps");
 			}
+		}
+
+		if (options.ignoreData.get() && connected == false)
+		{
+			connected = true;
 		}
 
 		return connected;
