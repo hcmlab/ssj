b5223bed9e9f03626136a4ccecb0fc1b55da4758
Vitaly <vitalijskrumins@gmail.com>
2017-11-28 15:05:18 +0100

    implemented mp4 decoder

--- ssjcreator/src/main/java/hcm/ssj/creator/activity/GraphActivity.java
+++ ssjcreator/src/main/java/hcm/ssj/creator/activity/GraphActivity.java
@@ -27,19 +27,28 @@
 
 package hcm.ssj.creator.activity;
 
+import android.media.AudioTrack;
+import android.media.MediaCodec;
+import android.media.MediaCodec.BufferInfo;
+import android.media.MediaExtractor;
+import android.media.MediaFormat;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
 import com.obsez.android.lib.filechooser.ChooserDialog;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
 
 import hcm.ssj.creator.R;
 import hcm.ssj.creator.main.GraphDrawer;
+import hcm.ssj.file.FileCons;
 
 /**
  * Visualize user-saved stream file data with the GraphView.
@@ -68,12 +77,105 @@ public class GraphActivity extends AppCompatActivity
 				chooserDialog.withChosenListener(new ChooserDialog.Result() {
 					@Override
 					public void onChoosePath(String path, File pathFile) {
+						/*
 						GraphDrawer drawer = new GraphDrawer(graph);
 						drawer.drawGraph(pathFile);
+						*/
+						try
+						{
+							decode(pathFile.getPath());
+						}
+						catch (Exception e)
+						{
+							e.printStackTrace();
+						}
 					}
 				}).build();
 				chooserDialog.show();
 			}
 		});
+	}
+
+	private void decode(String filepath) throws Exception
+	{
+		// Set audio source for the extractor.
+		MediaExtractor extractor = new MediaExtractor();
+		extractor.setDataSource(filepath);
+
+		// Get audio format.
+		MediaFormat format = extractor.getTrackFormat(0);
+		String mime = format.getString(MediaFormat.KEY_MIME);
+
+		// Create and configure decoder based on audio format.
+		MediaCodec decoder = MediaCodec.createDecoderByType(mime);
+		decoder.configure(format, null, null, 0);
+		decoder.start();
+
+		// Create input/output buffers.
+		ByteBuffer[] inputBuffers = decoder.getInputBuffers();
+		ByteBuffer[] outputBuffers = decoder.getOutputBuffers();
+		BufferInfo bufferInfo = new BufferInfo();
+		extractor.selectTrack(0);
+
+		File dst = new File(FileCons.SSJ_EXTERNAL_STORAGE + File.separator + "output.raw");
+		FileOutputStream f = new FileOutputStream(dst);
+
+		boolean endOfStreamReached = false;
+
+		while (true)
+		{
+			if (!endOfStreamReached)
+			{
+				int inputBufferIndex = decoder.dequeueInputBuffer(10 * 1000);
+				if (inputBufferIndex >= 0)
+				{
+					ByteBuffer inputBuffer = inputBuffers[inputBufferIndex];
+					int sampleSize = extractor.readSampleData(inputBuffer, 0);
+					if (sampleSize < 0)
+					{
+						// Pass empty buffer and the end of stream flag to the codec.
+						decoder.queueInputBuffer(inputBufferIndex, 0, 0,
+											   0, MediaCodec.BUFFER_FLAG_END_OF_STREAM);
+						endOfStreamReached = true;
+					}
+					else
+					{
+						// Pass data-filled buffer to the decoder.
+						decoder.queueInputBuffer(inputBufferIndex, 0, sampleSize,
+											   extractor.getSampleTime(), 0);
+						extractor.advance();
+					}
+				}
+			}
+
+			int outputBufferIndex = decoder.dequeueOutputBuffer(bufferInfo, 10 * 1000);
+			if (outputBufferIndex >= 0)
+			{
+				ByteBuffer outputBuffer = outputBuffers[outputBufferIndex];
+				byte[] data = new byte[bufferInfo.size];
+				outputBuffer.get(data);
+				outputBuffer.clear();
+
+				if (data.length > 0)
+				{
+					f.write(data, 0, data.length);
+				}
+				decoder.releaseOutputBuffer(outputBufferIndex, false);
+
+				if ((bufferInfo.flags & MediaCodec.BUFFER_FLAG_END_OF_STREAM) != 0)
+				{
+					endOfStreamReached = true;
+				}
+			}
+			else if (outputBufferIndex == MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED)
+			{
+				outputBuffers = decoder.getOutputBuffers();
+			}
+
+			if ((bufferInfo.flags & MediaCodec.BUFFER_FLAG_END_OF_STREAM) != 0)
+			{
+				break;
+			}
+		}
 	}
 }
